6559634b1974bce5abef0f9ec9606206f0190374
marie-george <maria85night@gmail.com>
2023-02-17 15:17:47 +0300

    made all in one function

--- main_code.py
+++ main_code.py
@@ -1,8 +1,16 @@
 from packages.bank_data_preparation import*
 from packages.class_transaction import*
+from packages.json_export import*
 
-bank_data_short = first_five_transactions()
 
-for i in bank_data_short:
-    transaction = Transaction(i['date'], i['description'], i['from'], i['to'], i['operationAmount'])
-    print(f'{transaction.format_date()} {transaction.descr}\n{transaction.format_from()} -> {transaction.format_to()}\n{transaction.format_amount()}\n')
+def main():
+    bank_data = get_bank_data()
+    bank_data = is_executed(bank_data)
+    bank_data = date_sorted(bank_data)
+    bank_data = is_from_absent(bank_data)
+    bank_data = first_five_transactions(bank_data)
+    for i in bank_data:
+        transaction = Transaction(i['date'], i['description'], i['from'], i['to'], i['operationAmount'])
+        print(f'{transaction.format_date()} {transaction.descr}\n{transaction.format_from()} -> {transaction.format_to()}\n{transaction.format_amount()}\n')
+
+main()
